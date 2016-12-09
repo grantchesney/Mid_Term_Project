b68138adfd2fbf3346e25d5a51883168cf1a91b1
Grant Chesney <grantchesney@crescentschool.org>
2016-12-09 17:54:39 -0500

    Working on the game logic and the specific grid for connect four.

--- src/Game/Connect_Four/Connect_Four_Grid.java
+++ src/Game/Connect_Four/Connect_Four_Grid.java
@@ -4,6 +4,15 @@ import Game.Grid;
 
 public class Connect_Four_Grid extends Grid {
 
-	
-	
+	public void addToColom(int column, String player) {
+
+		for (int i = 0; i < this.getHeight(); i++) {
+			if (this.getElement(column, i) == null) {
+				this.addElement(column, i, player);
+				break;
+			}
+		}
+
+	}
+
 }
--- src/Game/Main.java
+++ src/Game/Main.java
@@ -1,10 +1,22 @@
 package Game;
 
+import Game.Connect_Four.Connect_Four_Grid;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Connect_Four_Grid g = new Connect_Four_Grid();
+		g.addToColom(0, "x");
+		g.addToColom(0, "y");
+		
+		System.out.println(g.getElement(0, 0));
+		System.out.println(g.getElement(0, 1));
+		System.out.println(g.getElement(0, 2));
+		System.out.println(g.getElement(0, 3));
+		System.out.println(g.getElement(0, 4));
+		System.out.println(g.getElement(0, 5));
+		System.out.println(g.getElement(0, 6));
+		System.out.println(g.getElement(0, 7));
 	}
 
 }
