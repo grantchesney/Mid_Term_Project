a338096e0db0e78823dc0451dc777aae1529f169
Drake Wong <drakewong@crescentschool.org>
2016-12-14 14:17:32 -0500

    Added images for the Othello game pieces. Created buffered images for them inside of  Othello class and drew the background in the paint method.

--- src/Game/Othello/Othello.java
+++ src/Game/Othello/Othello.java
@@ -3,15 +3,116 @@ package Game.Othello;
 import Game.Game;
 
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
 
-public class Othello extends Game{
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
 
-    public void run() {
+public class Othello extends Game implements KeyListener, MouseListener, MouseMotionListener {
+	URL fileURL; 
+	private Othello_Grid grid;
+	//buffered images
+	BufferedImage blackPiece;
+	BufferedImage whitePiece;
+	BufferedImage othelloBoard;
+	
+	boolean turn = false; 
+	//false: black	true: white
+	
+	public void run() {
+		
+	}
 
-    }
+	public void draw(Graphics g) {
+		//draw game board
+		g.drawImage(othelloBoard, 0, 0, null);
+	}
 
-    public void draw(Graphics g) {
+	public Othello() throws IOException  {
+		//image files 
+		fileURL = getClass().getResource("Othello board.png");
+		othelloBoard = ImageIO.read(fileURL);
+		
+		fileURL = getClass().getResource("whitePiece.png");
+		whitePiece = ImageIO.read(fileURL);
+		
+		fileURL = getClass().getResource("blackPiece.png");
+		blackPiece = ImageIO.read(fileURL);
+		
+		grid = new Othello_Grid();
+	}
 
-    }
+	
+	
+	
+	//----------Auto Generated--------------
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 
 }
--- src/Game/Othello/Othello_Grid.java
+++ src/Game/Othello/Othello_Grid.java
@@ -3,5 +3,5 @@ package Game.Othello;
 import Game.Grid;
 
 public class Othello_Grid extends Grid{
-
+	
 }
