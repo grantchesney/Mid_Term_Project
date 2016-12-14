71c48638cdf1784cf355b635d71fcf3619a93333
Drake Wong <drakewong@crescentschool.org>
2016-12-14 10:11:13 -0500

    Updated Othello class.

--- src/Game/Othello/Othello.java
+++ src/Game/Othello/Othello.java
@@ -3,15 +3,95 @@ package Game.Othello;
 import Game.Game;
 
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
-public class Othello extends Game{
+import javax.swing.JPanel;
 
-    public void run() {
+public class Othello extends Game implements KeyListener, MouseListener, MouseMotionListener {
 
-    }
+	private Othello_Grid grid;
 
-    public void draw(Graphics g) {
+	public void run() {
 
-    }
+	}
+
+	public void draw(Graphics g) {
+
+	}
+
+	public Othello()  {
+
+		grid = new Othello_Grid();
+	}
+
+	
+	
+	
+	//----------Auto Generated--------------
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 
 }
