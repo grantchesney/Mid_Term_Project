88be6affb454141a4104660809f5ae85a2e9fbdf
Drake Wong <drakewong@crescentschool.org>
2016-12-14 19:04:19 -0500

    Implemented code for elements to be added to the grid array when grid is clicked in Othello class.

--- src/Game/Othello/Othello.java
+++ src/Game/Othello/Othello.java
@@ -8,23 +8,41 @@ import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
 
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 public class Othello extends Game implements KeyListener, MouseListener, MouseMotionListener {
-
+	URL fileURL; 
 	private Othello_Grid grid;
-
+	//buffered images
+	BufferedImage blackPiece;
+	BufferedImage whitePiece;
+	BufferedImage othelloBoard;
+	
 	public void run() {
-
+		
 	}
 
 	public void draw(Graphics g) {
-
+		//draw game board
+		g.drawImage(othelloBoard, 0, 0, null);
 	}
 
-	public Othello()  {
-
+	public Othello() throws IOException  {
+		//image files 
+		fileURL = getClass().getResource("Othello board.png");
+		othelloBoard = ImageIO.read(fileURL);
+		
+		fileURL = getClass().getResource("whitePiece.png");
+		whitePiece = ImageIO.read(fileURL);
+		
+		fileURL = getClass().getResource("blackPiece.png");
+		blackPiece = ImageIO.read(fileURL);
+		
 		grid = new Othello_Grid();
 	}
 
@@ -48,6 +66,16 @@ public class Othello extends Game implements KeyListener, MouseListener, MouseMo
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
+		int clickX = MouseInfo.getPointerInfo().getLocation().x;
+		int clickY = MouseInfo.getPointerInfo().getLocation().y;
+		int gridX, gridY;
+		//if mouse is clicked on game board
+		if (clickX < 875 && clickX > 155 && clickY > -750 && clickY < -30){
+			//find grid location that was clicked
+			gridX = (clickX - 155)/90 + 1;
+			gridY = (-clickY - 30)/90 + 1;
+			grid.addElement(gridX, gridY, grid.playerTurn);
+		}
 		
 	}
 
